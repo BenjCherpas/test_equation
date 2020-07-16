65bfae86c8ca60592e13e8ee0c5eb781be72025c
benjapidev <benjamin.cherpas@apidev.fr>
2020-07-16 23:28:46 +0200

    initial commit

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+
+class User
+{
+    private $_number;
+    private $_firstname;
+    private $_lastname;
+
+    public function extractData($file_path)
+    {
+
+    }
+}
+
+class Call
+{
+    private $_number;
+    private $_begin_date;
+    private $_end_date;
+    private $_duration;
+
+    public function extractData($file_path)
+    {
+
+    }
+
+    public function afficherDonnees()
+    {
+
+    }
+}
+
+?>
