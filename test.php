f78a38e6b5724d994235bfd8eb0f86fc2a6a974d
Benjamin Cherpas <benjcherpas@orange.fr>
2020-07-21 13:32:21 +0200

    second release

--- test.php
+++ test.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <title>Test EQUATION</title>
     <style>
-        table, tr, th, td {
+        table, tr, td, th {
             padding: 5px;
             border: thin solid black;
+            border-collapse: collapse;
         }
     </style>
 </head>
 <body>
-<div style="border: thin solid black; width: 1000px; padding: 15px;">
+<div style="width: auto; padding: 15px;">
 
 <?php
 
@@ -19,6 +20,7 @@ class User
 {
     public $_users = array();
 
+    // On crée un premier tableau {numero => {prenom, nom}}
     public function getUsers()
     {
         $row = 1;
@@ -26,11 +28,12 @@ class User
         {
             while (($data = fgetcsv($handle, 1000, ";")) !== FALSE)
             {
-                array_push($this->_users,  array($data[0], $data[1], $data[2]));
+                $this->_users[$data[0]] =  array($data[1], $data[2]);
                 $row++;
             }
             fclose($handle);
         }
+
         return $this->_users;
     }
 }
@@ -43,11 +46,14 @@ class Call
     protected $_end_date;
     protected $_duration;
     protected $_users = array();
+    protected $_call = array();
 
-    public function extractData()
+    // On ajout chaque donnée d'appel au numero correspondant sous forme de tableau de tableau
+    public function createArray()
     {
         $row = 1;
 
+        // {numero => {prenom, nom, {{appel1}, {appel2}, ...}}}
         if (($handle = fopen('platform1.csv', "r")) !== FALSE)
         {
             $users = new User();
@@ -65,69 +71,71 @@ class Call
                 $this->_end_date = $this->_end_date->format("d/m/Y H:i:s");
                 $this->_duration = explode(" ", $this->_duration, "2")[1];
 
-                for($i = 0; $i < count($this->_users); $i++)
-                {
-                    if(in_array($this->_number, array($this->_users[$i][0])))
-                    {
-                        array_push($this->_users[$i], $this->_start_date, $this->_end_date, $this->_duration);
-                    }
+                array_push($this->_call, array($this->_number, $this->_start_date, $this->_end_date, $this->_duration));
 
-                }
                 $row++;
             }
             fclose($handle);
-//            print_r($this->_users);
         }
 
-//        if (($handle = fopen('platform2.csv', "r")) !== FALSE)
-//        {
-//            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE)
-//            {
-//                $this->_number = $data[0];
-//                $this->_end_date = $data[1] + ($data[2]* 60);
-//                $this->_end_date = date("d/m/Y H:i:s", $this->_end_date);
-//                $this->_start_date = date("d/m/Y H:i:s", $data[1]);
-//                $this->_duration = gmdate("H:i:s", $data[2] * 60);
-//                $this->_duration = date_create_from_format('d/m/Y H:i:s', "00/00/0000 " . $this->_duration);
-//                $this->_duration = $this->_duration->format("H:i:s");
-//
-//                array_push($this->_users[$this->_number], $this->_start_date, $this->_end_date, $this->_duration);
-//
-//                $row++;
-//            }
-//            fclose($handle);
-//
-//        }
+        // Comme au dessus mais avec les 2e fichier
+        if (($handle = fopen('platform2.csv', "r")) !== FALSE)
+        {
+            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE)
+            {
+                $this->_number = $data[0];
+                $this->_end_date = $data[1] + ($data[2]* 60);
+                $this->_end_date = date("d/m/Y H:i:s", $this->_end_date);
+                $this->_start_date = date("d/m/Y H:i:s", $data[1]);
+                $this->_duration = gmdate("H:i:s", $data[2] * 60);
+                $this->_duration = date_create_from_format('d/m/Y H:i:s', "00/00/0000 " . $this->_duration);
+                $this->_duration = $this->_duration->format("H:i:s");
+
+                array_push($this->_call, array($this->_number, $this->_start_date, $this->_end_date, $this->_duration));
+
+                $row++;
+            }
+            fclose($handle);
+        }
+
+        // Initialisation d'un tableau en 3e position (tableau contenant les tableaux des appels)
+        foreach ($this->_users as $key => $value)
+        {
+            $this->_users[$key][2] = array();
+        }
+
+        foreach($this->_call as $elt)
+        {
+            array_push($this->_users[$elt[0]][2], array($elt[1], $elt[2], $elt[3]));
+        }
+
+        return $this->_users;
     }
 
-//    public function showFinalData()
-//    {
-//        $response = $bdd->query("SELECT u.firstname, u.lastname, u.number, c.start_date, c.end_date, c.duration
-//                                FROM call_phone c
-//                                INNER JOIN user u ON u.number=c.number
-//                                ORDER BY u.firstname, c.start_date ASC");
-//        $data = $response->fetchAll();
-//        echo "<table><tr><th>Nom</th><th>Prénom</th><th>Numéro</th><th>Date début d'appel</th><th>Date fin d'appel</th><th>Durée</th></tr>";
-//
-//        foreach ($data as $d)
-//        {
-//            echo "<tr><td>" . $d[0] . "</td><td>" . $d[1] . "</td><td>" . $d[2] . "</td><td>" . $d[3] . "</td><td>" . $d[4] . "</td><td>" . $d[5] . "</td></tr>";
-//        }
-//
-//        echo "</table>";
-//
-//    }
+    public function showFinalData()
+    {
+        $call = new Call();
+        $call = $call->createArray();
+        ksort($call);  // Tri du tableau principal (par numéro)
+
+        echo "<table><tr><th>Numéro</th><th>Prénom</th><th>Nom</th><th>Date début d'appel</th><th>Date fin d'appel</th><th>Durée</th></tr>";
+
+        foreach($call as $key => $value)
+        {
+            if(! empty($value[2])) // Ne pas garder les utilisateurs qui n'ont pas appelé
+            {
+                foreach ($value[2] as $elt)
+                {
+                    echo "<tr><td>" . $key . "</td><td>" . $value[0] . "</td><td>" . $value[1] . "</td><td>" . $elt[0] . "</td><td>" . $elt[1] . "</td><td>" . $elt[2] . "</td></tr>";
+                }
+            }
+        }
+        echo "</table>";
+    }
 }
 
-
-//$user = new User();
-//$user->getUsers();
-
 $call = new Call();
-$call->extractData();
-
-//$call = new Call();
-//$call->showFinalData();
+$call->showFinalData();
 
 ?>
 
